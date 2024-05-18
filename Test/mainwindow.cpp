9c5db82690e9996a3b848fbabfc1ea787286fb75
ZZQ <2300013153@stu.pku.edu.cn>
2024-05-18 12:36:35 +0800

    ZZQ's branch的第一次commit

--- Test/mainwindow.cpp
+++ Test/mainwindow.cpp
@@ -1,12 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QLabel>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->label->hide();
     QLabel *label1 = new QLabel();
     label1->setWindowTitle("pp");
     label1->resize(500,100);
