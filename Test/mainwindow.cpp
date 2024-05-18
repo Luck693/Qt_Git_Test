239b2221a4e899e3851bc1d7d453d4f653797043
ZZQ <2300013153@stu.pku.edu.cn>
2024-05-18 10:46:03 +0800

    这是commit到本地的git吗

--- Test/mainwindow.cpp
+++ Test/mainwindow.cpp
@@ -6,6 +6,13 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->label->hide();
+    QLabel *label1 = new QLabel();
+    label1->setWindowTitle("pp");
+    label1->resize(500,100);
+    label1->move(50, 50);
+    label1->show();
+
 }
 
 MainWindow::~MainWindow()
